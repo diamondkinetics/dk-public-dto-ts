466a3a5a026ea183d43616fd8630c86cd238423c
Eric Drobotij <edrobotij@diamondkinetics.com>
2020-01-14 09:49:31 -0500

    Added v3 index.

---
+++ src/lib/v3/index.ts
@@ -0,0 +1,5 @@
+export { BattingSessionDTOV3 } from './batting-session.dto.v3';
+export { SwingDTOV3 } from './swing.dto.v3';
+export { PitchingSessionDTOV3 } from './pitching-session.dto.v3';
+export { PitchDTOV3 } from './pitch.dto.v3';
+export { VideoDTOV3 } from './video.dto.v3';
