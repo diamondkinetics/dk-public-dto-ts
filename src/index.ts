791538e9c89226a738dbe82ce498ab143122986c
Eric Drobotij <edrobotij@diamondkinetics.com>
2020-01-17 09:36:46 -0500

    Following the same export pattern for enums as DTOs

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
+export * from './lib/enum';
 export * from './lib/v2';
 export * from './lib/v3';
---
+++ src/lib/enum/index.ts
@@ -0,0 +1,8 @@
+export { AccountStatus } from './account-status';
+export { AggregationOperation } from './aggregation-operation';
+export { BattingOrientation } from './batting-orientation';
+export { DKSubscription } from './dk-subscription';
+export { MaterialType } from './material-type';
+export { PitchType } from './pitch-type';
+export { Position } from './position';
+export { SessionType } from './session-type';
