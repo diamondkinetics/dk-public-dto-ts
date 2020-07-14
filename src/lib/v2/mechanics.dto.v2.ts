b246a5efc4e1e0c4914802bfe135ad3098c8287c
Eric Drobotij <edrobotij@diamondkinetics.com>
2020-07-14 08:13:43 -0400

    Removed DTO interfaces we don't need due to deprecation in the API.

--- src/lib/v2/mechanics.dto.v2.ts
+++
@@ -1,7 +0,0 @@
-import { AbstractSyncableDTOV2 } from './abstract-syncable.dto.v2';
-
-export interface MechanicsDTOV2 extends AbstractSyncableDTOV2 {
-	swingUuid?: string;
-	mechanicType: number;
-	value: string;
-}
