9f33cf92d9ec79ec2654267afedb55aa71c93f65
Eric Drobotij <edrobotij@diamondkinetics.com>
2020-07-14 08:15:48 -0400

    Added some more v2 DTO mocks.

---
+++ src/lib/mock/v2/index.ts
@@ -0,0 +1,4 @@
+export { abstractSyncableMockDTOV2 } from './abstract-syncable.mock.dto.v2';
+export { abstractTimestampMockDTOV2 } from './abstract-timestamp.mock.dto.v2';
+export { userAppVersionOverrideMockDTOV2 } from './user-app-version-override.mock.dto.v2';
+export { userProfileMockDTOV2 } from './user-profile.mock.dto.v2';
---
+++ src/lib/mock/v2/user-app-version-override.mock.dto.v2.ts
@@ -0,0 +1,12 @@
+import * as Factory from 'factory.ts';
+import * as faker from 'faker';
+
+import { UserAppVersionOverrideDTOV2 } from '~lib/v2';
+
+export const userAppVersionOverrideMockDTOV2 = Factory.Sync.makeFactory<UserAppVersionOverrideDTOV2>({
+	deleted: faker.random.boolean(),
+	effectiveStarting: faker.date.past(1).toISOString(),
+	effectiveUntil: faker.date.past(0.5).toISOString(),
+	versionKey: faker.database.column(),
+	value: faker.system.semver()
+});
