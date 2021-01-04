696d3cc99bc52fea41b1dcf78937688b8801c010
Eric Drobotij <edrobotij@diamondkinetics.com>
2021-01-04 13:31:05 -0500

    Exporting AbstractSyncableDTO from the dto index.

--- src/lib/dto/index.ts
+++ src/lib/dto/index.ts
@@ -4,5 +4,6 @@ export * from './v3';
 export * from './v4';
 
 // Non-versioned
+export * from './abstract-syncable.dto';
 export * from './reset-password-request.dto';
 export * from './logged-in-user.dto';
