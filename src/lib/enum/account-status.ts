6c65b8ef357bd0e7b788036ed1ba4df559712a4b
Eric Drobotij <edrobotij@diamondkinetics.com>
2021-01-04 13:35:00 -0500

    Added a name field to the AccountStatus enum.

--- src/lib/enum/account-status.ts
+++ src/lib/enum/account-status.ts
@@ -1,22 +1,28 @@
 export class AccountStatus {
 
-	public static PRE_TRIAL = new AccountStatus(true, true);
-	public static PRE_TRIAL_WITH_CREDIT_CARD = new AccountStatus(true, true);
-	public static TRIALING = new AccountStatus(true, true);
-	public static TRIALING_WITH_CREDIT_CARD = new AccountStatus(true, true);
-	public static ABOUT_TO_END_TRIAL = new AccountStatus(true, true);
-	public static TRIAL_ENDED = new AccountStatus(true, true);
-	public static PAID = new AccountStatus(false, false);
-	public static EXPIRED = new AccountStatus(false, false);
-	public static CANCELLED = new AccountStatus(false, false);
-	public static PAST_DUE = new AccountStatus(false, false);
+	public static PRE_TRIAL = new AccountStatus('PRE_TRIAL', true, true);
+	public static PRE_TRIAL_WITH_CREDIT_CARD = new AccountStatus('PRE_TRIAL_WITH_CREDIT_CARD', true, true);
+	public static TRIALING = new AccountStatus('TRIALING', true, true);
+	public static TRIALING_WITH_CREDIT_CARD = new AccountStatus('TRIALING_WITH_CREDIT_CARD', true, true);
+	public static ABOUT_TO_END_TRIAL = new AccountStatus('ABOUT_TO_END_TRIAL', true, true);
+	public static TRIAL_ENDED = new AccountStatus('TRIAL_ENDED', true, true);
+	public static PAID = new AccountStatus('PAID', false, false);
+	public static EXPIRED = new AccountStatus('EXPIRED', false, false);
+	public static CANCELLED = new AccountStatus('CANCELLED', false, false);
+	public static PAST_DUE = new AccountStatus('PAST_DUE', false, false);
 
+	private readonly name: string;
 	private readonly trialing: boolean;
 	private readonly paid: boolean;
 
-	constructor(trialing: boolean, paid: boolean) {
+	constructor(name: string, trialing: boolean, paid: boolean) {
+		this.name = name;
 		this.trialing = trialing;
 		this.paid = paid;
+	}
+
+	get getName(): string {
+		return this.name;
 	}
 
 	get isTrialing(): boolean {
