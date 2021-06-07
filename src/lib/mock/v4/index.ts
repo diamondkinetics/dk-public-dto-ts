c792f565c7b26470bb367fbf01590e68646929ea
Eric Drobotij <edrobotij@diamondkinetics.com>
2021-06-07 12:16:48 -0400

    Added GroupMembershipSummaryDTOV4 mock

--- src/lib/mock/v4/index.ts
+++ src/lib/mock/v4/index.ts
@@ -3,3 +3,4 @@ export * from './abstract-syncable.dto.v4.mock';
 export * from './group.dto.v4.mock';
 export * from './group-summary.dto.v4.mock';
 export * from './group-membership.dto.v4.mock';
+export * from './group-membership-summary.dto.v4.mock';
