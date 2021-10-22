04d90280c70a762c0ac921848a7838c9fa0a4b1b
Edward Lach <edwarddlach@gmail.com>
2021-10-22 11:40:02 -0400

    Removed Syncable Collection response V5 from AbstractSyncableDTO type because its collected nature does not follow the spirit of the type and is missing a uuid field

--- src/lib/dto/abstract-syncable.dto.ts
+++ src/lib/dto/abstract-syncable.dto.ts
@@ -4,4 +4,4 @@ import { AbstractSyncableDTOV4 } from './v4';
 import { AbstractSyncableCollectionResponseSummaryV5, AbstractSyncableResponseV5 } from "./v5/abstract-syncable.dto.v5";
 
 export type AbstractSyncableDTO = AbstractSyncableDTOV2 | AbstractActivityDTOV3 | AbstractSyncableDTOV4
-    | AbstractSyncableResponseV5 | AbstractSyncableCollectionResponseSummaryV5;
+    | AbstractSyncableResponseV5;
