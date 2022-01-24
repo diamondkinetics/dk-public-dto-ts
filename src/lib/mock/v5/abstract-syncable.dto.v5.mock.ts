3d37adb3689c6a567086a636320c01fa8b6a34ad
Eric Drobotij <edrobotij@diamondkinetics.com>
2022-01-24 10:01:44 -0500

    Trying to use functions from now on. The same value gets used for multiple mocks if this approach is not taken.

--- src/lib/mock/v5/abstract-syncable.dto.v5.mock.ts
+++ src/lib/mock/v5/abstract-syncable.dto.v5.mock.ts
@@ -9,21 +9,20 @@ import { mockAbstractTimeStampResponseV5 } from "./abstract-timestamp.dto.v5.moc
 import { mockCollectionResponseSummaryV5 } from "./collection.dto.v5.mock";
 
 export const mockAbstractSyncableUpdateRequestV5 = cookyCutter.define<AbstractSyncableUpdateRequestV5>({
-    clientUpdatedTime: faker.date.recent(1).toISOString(),
-    deleted: faker.random.boolean()
+    clientUpdatedTime: () => faker.date.recent(1).toISOString(),
+    deleted: () => faker.random.boolean()
 });
 
 export const mockAbstractSyncableResponseV5 = cookyCutter.define<AbstractSyncableResponseV5>({
     ...mockAbstractTimeStampResponseV5(),
-    clientCreatedTime: faker.date.past(1).toISOString(),
-    clientUpdatedTime: faker.date.recent(1).toISOString(),
-    uuid: faker.random.uuid(),
-    deleted: faker.random.boolean()
+    clientCreatedTime: () => faker.date.past(1).toISOString(),
+    clientUpdatedTime: () => faker.date.recent(1).toISOString(),
+    uuid: () => faker.random.uuid(),
+    deleted: () => faker.random.boolean()
 });
 
 export const mockAbstractSyncableCollectionResponseSummaryV5 = cookyCutter.define<AbstractSyncableCollectionResponseSummaryV5>({
     ...mockCollectionResponseSummaryV5(),
     countDeleted: 1,
-    maxLastUpdated: faker.date.recent(7).toISOString()
+    maxLastUpdated: () => faker.date.recent(7).toISOString()
 });
-
