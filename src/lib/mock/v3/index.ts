c33d00849418652153dff22c16a4177bddda576d
Eric Drobotij <edrobotij@diamondkinetics.com>
2022-01-26 10:14:25 -0500

    Forgot to export the pending group import mocks.

--- src/lib/mock/v3/index.ts
+++ src/lib/mock/v3/index.ts
@@ -6,3 +6,6 @@ export * from './user-profile.dto.v3.mock';
 export * from './user-profile-basics.dto.v3.mock';
 export * from './video.dto.v3.mock';
 export * from './export-request.dto.v3.mock';
+export * from './pending-group-import.dto.v3.mock';
+export * from './pending-group-import-row.dto.v3.mock';
+export * from './pending-group-import-conflict.dto.v3.mock';
