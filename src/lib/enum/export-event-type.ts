600d697673ed029f3f5148e11c45f112cc3db510
Eric Drobotij <edrobotij@diamondkinetics.com>
2022-04-07 14:52:26 -0400

    Added a templateId property to the export event type enum to support the Swings Lite export template.

--- src/lib/enum/export-event-type.ts
+++ src/lib/enum/export-event-type.ts
@@ -1,13 +1,17 @@
 export class ExportEventType {
 
 	public static readonly SWINGS = new ExportEventType('Swings');
-	public static readonly SWINGS_LITE = new ExportEventType('Swings Lite');
+	public static readonly SWINGS_LITE = new ExportEventType('Swings Lite', '1WBsr5zWLESxwQp786mCvuy8SS2ck12j9leQwbgDNPPY');
 	public static readonly PITCHES = new ExportEventType('Pitches');
 
-	constructor(private readonly name: string) {}
+	constructor(private readonly name: string, private readonly templateId?: string) {}
 
 	public getName(): string {
 		return this.name;
+	}
+
+	public getTemplateId(): string | undefined {
+		return this.templateId;
 	}
 
 	public static asArray(): ExportEventType[] {
