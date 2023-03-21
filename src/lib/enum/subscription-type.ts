d68b6b14a6e25a162d4800239d2f4e2d50555c33
Eric Drobotij <edrobotij@diamondkinetics.com>
2023-03-21 00:58:13 -0400

    Adding DK_PLUS SubscriptionType.

--- src/lib/enum/subscription-type.ts
+++ src/lib/enum/subscription-type.ts
@@ -2,6 +2,7 @@ export class SubscriptionType {
 
 	public static HITTING = new SubscriptionType('HITTING');
 	public static PITCHING = new SubscriptionType('PITCHING');
+	public static DK_PLUS = new SubscriptionType('DK_PLUS');
 
 	constructor(private readonly name: string) {}
 
@@ -12,8 +13,8 @@ export class SubscriptionType {
 	public static asArray(): SubscriptionType[] {
 		return [
 			SubscriptionType.HITTING,
-			SubscriptionType.PITCHING
+			SubscriptionType.PITCHING,
+			SubscriptionType.DK_PLUS
 		];
 	}
-
 }
