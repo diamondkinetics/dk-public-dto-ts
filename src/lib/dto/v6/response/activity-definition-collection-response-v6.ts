053bfe0c5ed74e4296f1bfec27ed5a53964291ca
Evan Jenkins <ejenkins@diamond.kinetics.com>
2023-06-20 16:41:48 -0400

    added two new DTOs for activity definition responses, one for collections and one for singular activities. Updated index.ts file to reflect chages, as well as increased version in package.json to 0.3.3

---
+++ src/lib/dto/v6/response/activity-definition-collection-response-v6.ts
@@ -0,0 +1,4 @@
+import { CollectionResponseV6 } from './collection-response-v6';
+import { ActivityDefinitionResponseV6 } from './activity-definition-response-v6';
+
+export interface ActivityDefinitionCollectionResponseV6 extends CollectionResponseV6<ActivityDefinitionResponseV6> {}
