abe4af792ebfe582765264449d73ba55b37c8925
William Gay <wgay@diamondkinetics.com>
2023-07-13 10:40:31 -0400

    Added the V6 DTO interface related to the ViewEntryDTOV6 class in the API.

---
+++ src/lib/dto/v6/request/view-entry-create-request-v6.ts
@@ -0,0 +1,8 @@
+import { AbstractSyncableCreateRequestV6 } from "./abstract-syncable-create-request-v6";
+
+export interface ViewEntryCreateRequestV6 extends AbstractSyncableCreateRequestV6 {
+  viewSectionUuid: string;
+  thumbnailUrl: string;
+  displayOrder: number;
+  activityDefinitionUuid: string;
+}
