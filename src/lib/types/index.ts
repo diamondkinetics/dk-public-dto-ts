e78db5a3e07e98e4f8ee3c2f9effe0cae281f2ea
Eric Drobotij <edrobotij@diamondkinetics.com>
2023-07-28 16:46:06 -0400

    Exporting the array constants that drive the type values.

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,8 +1,8 @@
-export { ActivityRewardType } from './activity-reward-type';
-export { AdvanceConditionType } from './advance-condition-type';
-export { CriteriaType } from './criteria-type';
-export { DisplayChart } from './display-chart';
-export { DisplayMetric } from './display-metric';
-export { EngineType } from './engine-type';
-export { EventSource } from './event-source';
-export { VideoBlockType } from './video-block-type';
+export { ActivityRewardTypes, ActivityRewardType } from './activity-reward-type';
+export { AdvanceConditionTypes, AdvanceConditionType } from './advance-condition-type';
+export { CriteriaTypes, CriteriaType } from './criteria-type';
+export { DisplayCharts, DisplayChart } from './display-chart';
+export { DisplayMetrics, DisplayMetric } from './display-metric';
+export { EngineTypes, EngineType } from './engine-type';
+export { EventSources, EventSource } from './event-source';
+export { VideoBlockTypes, VideoBlockType } from './video-block-type';
