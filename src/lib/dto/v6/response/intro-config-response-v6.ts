824bb4d7be8d1b5f2edcc9066aa2d3c27db642ab
Eric Drobotij <edrobotij@diamondkinetics.com>
2023-07-28 09:28:15 -0400

    Fixing case issue from URL to Url on intro config coverImageUrl

--- src/lib/dto/v6/response/intro-config-response-v6.ts
+++ src/lib/dto/v6/response/intro-config-response-v6.ts
@@ -1,7 +1,7 @@
 export interface IntroConfigResponseV6 { 
   introVideoUrl: string;
   introVideoThumbnail: string;
-  coverImageURL: string;
+  coverImageUrl: string;
   description: string;
   aboutImageUrls: string[];
   aboutDescription: string;
