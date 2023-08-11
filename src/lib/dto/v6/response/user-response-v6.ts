409dbcd5dd740d350bba6e55f6b474491855e1e7
Eric Drobotij <edrobotij@diamondkinetics.com>
2023-08-11 14:55:47 -0400

    UserResponseV6 now extending AbstractSyncableResponseV6

--- src/lib/dto/v6/response/user-response-v6.ts
+++ src/lib/dto/v6/response/user-response-v6.ts
@@ -1,4 +1,6 @@
-export interface UserResponseV6 {
+import { AbstractSyncableResponseV6 } from './abstract-syncable-response-v6';
+
+export interface UserResponseV6 extends AbstractSyncableResponseV6 {
   firstName: string;
   lastName: string;
   profileImageUrl: string;
