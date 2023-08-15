8c8f1b60e15f390f8119079520ff8570b5ef5dd2
Evan Jenkins <ejenkins@diamond.kinetics.com>
2023-08-15 12:08:00 -0400

    removing duplicate DTOs

--- src/lib/dto/v6/response/badges-collection-response-v6.ts
+++
@@ -1,4 +0,0 @@
-import { CollectionResponseV6 } from "./collection-response-v6";
-import { BadgesResponseV6 } from "./badges-response-v6";
-
-export interface BadgesCollectionResponseV6 extends CollectionResponseV6<BadgesResponseV6>{}
--- src/lib/dto/v6/response/card-designs-collection-response-v6.ts
+++
@@ -1,4 +0,0 @@
-import { CollectionResponseV6 } from "./collection-response-v6";
-import { CardDesignsResponseV6 } from "./card-designs-response-v6";
-
-export interface CardDesignsCollectionResponseV6 extends CollectionResponseV6<CardDesignsResponseV6>{}
