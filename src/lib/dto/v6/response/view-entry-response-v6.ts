e8f58a997ccd944404c8bcbfec3e9cc8f6e0f4b8
William Gay <wgay@diamondkinetics.com>
2023-08-17 16:10:10 -0400

    changed view section and view entry dtos to match java api dtos

--- src/lib/dto/v6/response/view-entry-response-v6.ts
+++ src/lib/dto/v6/response/view-entry-response-v6.ts
@@ -1,6 +1,7 @@
 import { AbstractTimeWindowResponseV6 } from "./abstract-timewindow-response-v6";
 
 export interface ViewEntryResponseV6 extends AbstractTimeWindowResponseV6 {
+  viewSectionUuid: string;
   thumbnailUrl: string;
   displayOrder: number;
   activityDefinitionUuid: string;
