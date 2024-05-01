cd6ce7c62ab97eaa1a308de4059a2f2f5d6eeade
Eric Drobotij <edrobotij@diamondkinetics.com>
2024-05-01 09:52:39 -0400

    Deletes all mocks

--- src/lib/mock/v3/abstract-timestamp.dto.v3.mock.ts
+++
@@ -1,9 +0,0 @@
-import { define } from 'cooky-cutter';
-import { date } from 'faker';
-
-import { AbstractTimestampDTOV3 } from './../../dto/v3/abstract-timestamp.dto.v3';
-
-export const mockAbstractTimestampDTOV3 = define<AbstractTimestampDTOV3>({
-	creationDate: () => date.past(1).toISOString(),
-	lastUpdate: () => date.past(0.5).toISOString()
-});
--- src/lib/mock/v4/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './abstract-timestamp.dto.v4.mock';
-export * from './abstract-syncable.dto.v4.mock';
-export * from './group.dto.v4.mock';
-export * from './group-summary.dto.v4.mock';
-export * from './group-membership.dto.v4.mock';
-export * from './group-membership-summary.dto.v4.mock';
