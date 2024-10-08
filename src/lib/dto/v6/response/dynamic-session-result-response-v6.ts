e2515f1bb0544ffc76829d887e2cfa4f9b66c303
Eric Drobotij <edrobotij@diamondkinetics.com>
2024-10-08 09:27:52 -0400

    Updates dynamic session result related DTOs to extend user owned abstract DTOs.

--- src/lib/dto/v6/response/dynamic-session-result-response-v6.ts
+++ src/lib/dto/v6/response/dynamic-session-result-response-v6.ts
@@ -1,7 +1,7 @@
-import { AbstractSyncableResponseV6 } from './abstract-syncable-response-v6';
+import { AbstractUserOwnedResponseV6 } from './abstract-user-owned-response-v6';
 import { DynamicSessionBlockResultResponseV6 } from './dynamic-session-block-result-response-v6';
 
-export interface DynamicSessionResultResponseV6 extends AbstractSyncableResponseV6 {
+export interface DynamicSessionResultResponseV6 extends AbstractUserOwnedResponseV6 {
   activityUuid: string;
   blocks: DynamicSessionBlockResultResponseV6[];
 }
